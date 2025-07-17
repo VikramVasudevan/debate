55a8e955b766faa76f4cb73f097a1584001a045b
Vikram Vasudevan <vikram.vasudevan@ekahaa.com>
2025-07-17 11:55:01 +0530

    initial version

---
+++ app.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+import gradio as gr
+from src.debate.crew import Debate
+
+PROPOSITION_DEFAULT = "### Arguments in favor\n\n_Arguments in favor will appear here_"
+OPPOSITION_DEFAULT = "### Arguments against\n\n_Arguments against will appear here_"
+DECISION_DEFAULT = "### The Judge's Decision\n\n_Decision will appear here_"
+
+def debate_motion(motion):
+    debate = Debate()
+    crew = debate.crew()
+    inputs = {
+        'motion': motion
+    }
+    results = crew.kickoff(inputs=inputs)
+    print("Results: ", results)
+    proposition = debate.my_proposition_task.output.raw
+    opposition = debate.my_opposition_task.output.raw
+    decision = debate.my_judge_task.output.raw
+
+    yield proposition, opposition, decision
+
+
+if __name__ == "__main__":
+    load_dotenv(override=True)
+    with gr.Blocks(theme=gr.themes.Soft()) as demo:
+        motion = gr.Text(
+            label="Motion",
+            value="Being Vegan is better for the environment",
+            submit_btn="Start Debate",
+        )
+
+        with gr.Column():
+            with gr.Row(equal_height=True):
+                proposition = gr.Markdown(
+                    value=PROPOSITION_DEFAULT,
+                    container=True,
+                )
+
+                opposition = gr.Markdown(
+                    value=OPPOSITION_DEFAULT,
+                    container=True,
+                )
+
+            decision = gr.Markdown(
+                value=DECISION_DEFAULT,
+                container=True,
+            )
+
+        motion.submit(
+            debate_motion, inputs=[motion], outputs=[proposition, opposition, decision]
+        )
+    demo.launch()
---
+++ src/debate/crew.py
@@ -0,0 +1,78 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+
+@CrewBase
+class Debate:
+    """Debate crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def debate_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["debate_agent"],  # type: ignore[index]
+            verbose=True,
+        )
+
+    @agent
+    def judge_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["judge_agent"],  # type: ignore[index]
+            verbose=True,
+        )
+
+    @task
+    def proposition_task(self) -> Task:
+        self.my_proposition_task = Task(
+            config=self.tasks_config["proposition_task"],  # type: ignore[index]
+            output_key="proposition",
+            markdown=True
+        )
+        return self.my_proposition_task
+
+    @task
+    def opposition_task(self) -> Task:
+        self.my_opposition_task = Task(
+            config=self.tasks_config["opposition_task"],  # type: ignore[index]
+            output_key="opposition",
+            markdown=True
+        )
+        return self.my_opposition_task
+
+    @task
+    def judge_task(self) -> Task:
+        self.my_judge_task = Task(
+            config=self.tasks_config["judge_task"],  # type: ignore[index]
+            output_file="output/decision.md",
+            output_key="decision",
+            markdown=True
+        )
+        return self.my_judge_task
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Debate crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
